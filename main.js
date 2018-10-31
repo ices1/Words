8f86078ac94b35c720ae072ac1c9ed5b34163b2c
ices1 <iceesong@outlook.com>
2018-10-31 17:09:34 +0800

    done :fire:

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ let listRemember = document.querySelector('.list-remember')
 let toForgot = document.querySelector('.to-forgot')
 let toALittle = document.querySelector('.to-a-little')
 let toRemember = document.querySelector('.to-remember')
+let tipsWrap = document.querySelector('.tips-wrap')
 
-let searchDomain = 'https://vps.iceeweb.com:10002'
+// let searchDomain = 'https://vps.iceeweb.com:10002'
+let searchDomain = 'http://vps.iceeweb.com:10001'
 let active
 
 
@@ -17,13 +19,28 @@ let active
 
 
 // 初始化 store 到 Dom
-let pullStore = JSON.parse(localStorage.getItem('words')) || {}
+// let pullStore = JSON.parse(localStorage.getItem('words')) || {}
+
+// 测试 样本用例 store
+let pullStore = JSON.parse(localStorage.getItem('words')) || {You: "r",
+api: "r",badge: "r",bit: "r",button: "r",class: "r",content: "r",crossorigin: "l",
+div: "f",js: "l",justify: "r",latest: "r",li: "l",link: "f",message: "l",meta: "l",
+msg: "r",name: "l",nodejs: "r",org: "f",script: "l",static: "l",text: "r",xl: "l"}
+
 
 // 更新 Dom 元素 到 Store
 let pushStore = (word, status) => {
   let store = JSON.parse(localStorage.getItem('words')) || {}
   store[word] = status
   localStorage.setItem('words', JSON.stringify(store))
+}
+let delStore = (word) => {
+  console.log(word)
+  let store = JSON.parse(localStorage.getItem('words')) || {}
+  console.log(store)
+  delete store[word]
+  localStorage.setItem('words', JSON.stringify(store))
+  console.log(store)
 }
 console.log(0000, pullStore)
 // console.log(pushStore)
@@ -34,6 +51,7 @@ console.log(0000, pullStore)
   let storeObj = storeCat()
   console.log(111111111,storeObj)
   updataStoreToDom(storeObj)
+  document.querySelector('.tips-cnt').classList.add('tips-cnt-show')
 })()
 
 // sotre 分类 函数
@@ -94,20 +112,47 @@ active = {
 
 // 监听全局点击事件
 words.addEventListener('click', e => {
-  // debugger
-  // console.log(e.target)
-  // let idName = e.target.id
   let className = e.target.classList[0]
 
   // debugger
   if (active[className]) {
     active[className](e.target)
   }
-  // active[clsName](e.target)
 })
 
 searchBtn.addEventListener('click', e => {
   getData()
+})
+inputUrl.addEventListener('keyup', e => {
+  // console.log(e.keyCode)
+  if (e.keyCode === 13) {
+    getData()
+  }
+})
+tipsWrap.addEventListener('click', e => {
+  
+  let flag = document.querySelector('.tips-cnt-show')
+  console.log(flag)
+  if (!flag) {
+    document.querySelector('.tips-cnt').classList.add('tips-cnt-show')
+  } else {
+    document.querySelector('.tips-cnt').classList.remove('tips-cnt-show')
+  }
+})
+
+
+words.addEventListener('dblclick', e => {
+  let item = e.target
+  // console.log(e.target)
+
+  if (item.className === 'word-item') {
+    // 双击删除 item，添加样式动画
+    item.parentNode.classList.add('remove-item')
+    delStore(item.innerText)
+    setTimeout(() => {
+      item.parentNode.parentNode.removeChild(item.parentNode)
+    }, 200);
+  }
 })
 
 // 点击 searchBtn 获取数据 
@@ -115,11 +160,20 @@ async function  getData() {
   let frequency = []
   let url = inputUrl.value
   let words, data
+  console.log(searchDomain + '/?q=' + url)
 
-  await axios.get('http://localhost:10001/?q=' + 'http://localhost:8080/')
+  // await axios.get('http://localhost:10001/?q=' + 'http://localhost:8080/')
+  await axios.get(searchDomain + '/?q=' + url)
     .then((res) => {
-      data = res.data
-      console.log(data)
+      if (res.data.status === 'failed') {
+        console.log(res)
+        document.getElementsByClassName('message')[0].classList.add('message-show')
+        document.getElementsByClassName('message')[0].innerText = res.data.msg
+        document.getElementsByClassName('message')[0].style.color = 'red'
+      } else {
+        console.log(res)
+        wordSort(res.data)
+      }
     })
     .catch(err => {
       console.log(err)
@@ -127,6 +181,9 @@ async function  getData() {
 
   // 测试代码
   // let data = { words: 3, book: 5, eat: 1,words: 3, books: 5, ate: 1,word: 3, hi: 5, you: 1,me: 3, about: 5, and: 1, sleep: 3,sheep:8,dict:7 }
+}
+
+function wordSort(data) {
 
   // 拦截 以 过滤 存在 words
   let pureWords = filterWords(data)
@@ -183,7 +240,7 @@ function appendDom(dict) {
     // if (dict[0][i] in store) continue
 
     s += `<li class="list-group-item d-flex justify-content-between align-items-center">
-            <strong>${dict[0][i]}</strong>
+            <strong class='word-item'>${dict[0][i]}</strong>
             <span class="to-forgot cat-word rpos3">F</span>
             <span class="to-a-little cat-word rpos2">L</span>
             <span class="to-remember cat-word rpos1">R</span>
@@ -218,7 +275,10 @@ function removeItem(oper, el) {
   }
 
   word = el.parentNode.children[0].innerText
-  el.parentNode.parentNode.removeChild(el.parentNode)
+  el.parentNode.classList.add('remove-item')
+  setTimeout(() => {
+    el.parentNode.parentNode.removeChild(el.parentNode)
+  }, 200);
 
   // leftOne = oper == 'to-forgot' ? 'L' : 'F'
   // rightOne = oper == 'to-remember' ? 'L' : 'R'
@@ -233,7 +293,7 @@ function removeItem(oper, el) {
 
 function word2html(word, leftCls, leftOne, rightCls, rightOne) {
   return `<li class="list-group-item d-flex justify-content-between align-items-center">
-    <strong>${word}</strong>
+    <strong class='word-item'>${word}</strong>
     <span class="${leftCls} cat-word pos2 ">${leftOne}</span>
     <span class="${rightCls} cat-word pos1">${rightOne}</span></li>`
   }
